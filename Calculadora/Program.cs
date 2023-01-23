3a889b9153af847f2424bf62848392a16f03830e
Marco <51382551+maurelio631@users.noreply.github.com>
2023-01-23 15:23:44 +0000

    Criação dos métodos de subtração, divisão e multiplicação e adição deles no método Main

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -6,7 +6,10 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Soma();
+            // Soma();
+            Subtracao();
+            // Multiplicacao();
+            // Divisao();
 
         }
 
@@ -26,6 +29,61 @@ namespace Calculadora
             // Console.WriteLine("O resultado da soma é: " + soma);
             Console.WriteLine($"O resultado da soma é: {soma}");
             // Console.WriteLine($"O resultado da soma é: {primeiroValor + segundoValor}");
+        }
+
+        static void Subtracao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor:");
+            int primeiroValor = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int segundoValor = Int32.Parse(Console.ReadLine());
+
+            int subtracao = primeiroValor - segundoValor;
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da subtração é: {subtracao}");
+
+
+        }
+        static void Multiplicacao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor:");
+            int primeiroValor = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int segundoValor = Int32.Parse(Console.ReadLine());
+
+            int multiplicacao = primeiroValor * segundoValor;
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da subtração é: {multiplicacao}");
+
+
+        }
+        static void Divisao()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor:");
+            int primeiroValor = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int segundoValor = Int32.Parse(Console.ReadLine());
+
+            int divisao = primeiroValor - segundoValor;
+
+            Console.WriteLine("");
+
+            Console.WriteLine($"O resultado da subtração é: {divisao}");
+
+
         }
     }
 }
