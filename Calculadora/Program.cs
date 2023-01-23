7ea0c86ca37ff36a3ac370e3e1665b1f099e4700
Marco <51382551+maurelio631@users.noreply.github.com>
2023-01-23 15:11:12 +0000

    Criando a estrutura do projeto e fazendo os primeiros testes da calculadora

---
+++ Calculadora/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine("Digite o primeiro valor:");
+            int primeiroValor = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor:");
+            int segundoValor = Int32.Parse(Console.ReadLine());
+
+            int soma = primeiroValor + segundoValor;
+
+            Console.WriteLine("");
+
+            Console.WriteLine("O resultado da soma é: " + soma);
+            Console.WriteLine($"O resultado da soma é: {soma}");
+            Console.WriteLine($"O resultado da soma é: {primeiroValor + segundoValor}");
+
+        }
+    }
+}
